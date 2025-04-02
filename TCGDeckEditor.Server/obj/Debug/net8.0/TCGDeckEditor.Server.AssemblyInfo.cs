3ada2b6ceded94a1d097c01f116f87dde6529273
Luke Fosbrook <luke.fosbrook@outlook.com>
2025-04-02 19:44:00 +0100

    update

--- TCGDeckEditor.Server/obj/Debug/net8.0/TCGDeckEditor.Server.AssemblyInfo.cs
+++ TCGDeckEditor.Server/obj/Debug/net8.0/TCGDeckEditor.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TCGDeckEditor.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3fc331f153c59b9c2fd5472874ac6dc9340ac15")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1eba51541db227c274e6e0b8190fbadef2ccff6")]
 [assembly: System.Reflection.AssemblyProductAttribute("TCGDeckEditor.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TCGDeckEditor.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
